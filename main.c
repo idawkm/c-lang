ee571184af3cff54cb09309b53da8369f28666cd
mjxy9 <mjxy9@matheus.dev>
2024-05-16 20:35:34 -0300

    feat: first commit

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void run_file(char *argc);
+void run_prompt();
+void run(char *buffer);
+
+int main(int argc, char *argv[]) {
+
+    if (argc > 2) {
+        printf("usage: cinterpreter");
+        exit(64);
+    }
+
+    if (argc == 2) {
+        run_file(argv[1]);
+        return 0;
+    }
+
+    run_prompt();
+
+    return 0;
+}
+
+void run_file(char *path) {
+
+    FILE *file_ptr = fopen(path, "r+");
+
+    if (file_ptr == NULL) {
+        printf("could not open the file\n");
+        exit(1);
+    }
+
+    fseek(file_ptr, 0, SEEK_END);
+    long file_size = ftell(file_ptr);
+    rewind(file_ptr);
+
+    char *buffer = (char *) malloc(file_size * sizeof(char));
+    fread(buffer, file_size, 1, file_ptr);
+
+    run(buffer);
+    free(buffer);
+    fclose(file_ptr);
+}
+
+void run_prompt() {
+
+    while(1) {
+
+        printf("> ");
+        int size = 25;
+        char *line = (char *) malloc(size * sizeof(char));
+        fgets(line, size, stdin);
+
+        if (line == NULL) {
+            break;
+        }
+
+        run(line);
+        free(line);
+    }
+    printf("running the prompt\n");
+}
+
+void run(char *buffer) {
+
+    printf("%s\n", buffer);
+}
